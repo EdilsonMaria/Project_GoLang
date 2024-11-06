11784f7903fd30d805205d348e7949346156c4f2
edilson maria <edilson.eng1@gmail.com>
2024-11-06 11:27:35 -0300

    Refatoração do http-server

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
     "time"
 )
 
-//Response para representar os dados que vamos enviar como resposta ao cliente.
+//Define uma estrutura chamada Response, que representa a resposta enviada ao cliente.
 type Response struct {
     Nome    string `json:"nome"`
     Horario string `json:"horario"`
@@ -16,24 +16,26 @@ type Response struct {
 
 //Handler é chamada sempre que uma requisição é feita ao servidor.
 func handler(w http.ResponseWriter, r *http.Request) {
+
+    //Definição do fuso horário com base na localização
     location, err := time.LoadLocation("America/Sao_Paulo")
     if err != nil {
         http.Error(w, "Erro ao carregar o fuso horário", http.StatusInternalServerError)
         return
     }
 
-    //Define o horário de Brasília
+    //Criação da resposta
     response := Response{
         Nome:    "Projeto Korp",
         Horario: time.Now().In(location).Format(time.RFC3339),
     }
     
-	//Configurar e enviar a resposta
+	//Configurar e enviar a resposta (cabeçalho)
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(response)
 }
 
-
+//Define que, quando o servidor receber uma requisição no caminho raiz (/), ele chamará a função handler
 func main() {
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
